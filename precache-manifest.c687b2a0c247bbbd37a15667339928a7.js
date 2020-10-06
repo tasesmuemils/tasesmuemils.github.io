6b6490363798c211d65793006de3dacb43b461cd
EMILS <emils.bisenieks93@gmail.com>
2020-10-06 15:59:10 +0300

    Updates

---
+++ precache-manifest.c687b2a0c247bbbd37a15667339928a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cd7040b648273b52b21d8df1f249e0",
+    "url": "/projects/movies/index.html"
+  },
+  {
+    "revision": "fa306c2b51794dfef3b7",
+    "url": "/projects/movies/static/css/2.c8cfd04d.chunk.css"
+  },
+  {
+    "revision": "fa88f68ec9acbee770c8",
+    "url": "/projects/movies/static/css/main.4b40d3cb.chunk.css"
+  },
+  {
+    "revision": "fa306c2b51794dfef3b7",
+    "url": "/projects/movies/static/js/2.94506ff7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/projects/movies/static/js/2.94506ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa88f68ec9acbee770c8",
+    "url": "/projects/movies/static/js/main.de7f5580.chunk.js"
+  },
+  {
+    "revision": "02258730da024067fa1d",
+    "url": "/projects/movies/static/js/runtime-main.075a6818.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/projects/movies/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/projects/movies/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/projects/movies/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/projects/movies/static/media/slick.f97e3bbf.svg"
+  }
+]);
